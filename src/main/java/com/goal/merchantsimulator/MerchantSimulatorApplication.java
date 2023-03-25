1c4d21380ce3dbf42efe9b665733ba207d4c92e3
Devas <vardevvas@gmail.com>
2023-03-25 12:17:50 +0200

    first commit

---
+++ src/main/java/com/goal/merchantsimulator/MerchantSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.goal.merchantsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goal/merchantsimulator/MerchantSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goal.merchantsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
